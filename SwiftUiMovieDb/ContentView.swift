bd1cb60c64f4da184fb6aec1e4edcd8056f6ced1
Badar <badar.maulana.techno@gmail.com>
2020-11-02 20:23:09 +0700

    Initial Commit

---
+++ SwiftUiMovieDb/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUiMovieDb
+//
+//  Created by badar on 02/11/20.
+//  Copyright © 2020 badar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
